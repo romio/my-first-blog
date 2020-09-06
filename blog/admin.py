62ddde0108dc24d034a5840ac26ac448ef46eb97
Roman Konev <golfstreamsoft@gmail.com>
2020-09-06 21:45:14 +0600

    Шаблон блога с базовыми настройками

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+"""Comment"""
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,21 @@
+"""Comment"""
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    """Comment"""
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        """Comment"""
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        """Comment"""
+        return self.title
