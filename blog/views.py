35585aa859f24a8034753576a3e4f3a46da542ea
Roman Konev <golfstreamsoft@gmail.com>
2020-09-07 21:41:31 +0600

    Добавление шаблона detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 """Comment"""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,8 @@ def post_list(request):
     """Comment"""
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    """Comment"""
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
